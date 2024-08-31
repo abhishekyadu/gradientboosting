ba9cd7ee8d3e28e7ee3f1de8e06f088607558b8c
Abhishek Kumar Yadav <77387723+abhishekyadu@users.noreply.github.com>
2024-08-31 10:49:09 +0530

    Update gbr.py

--- gbr.py
+++ gbr.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 import pickle
 
